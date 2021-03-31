bed4214f4993a59e4cbe13f0ae01f641870e54a6
Jay Shin <75009488+popo2122@users.noreply.github.com>
2021-03-31 22:42:31 +0900

    modified main slide position before sliding

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(function () {
+  //toggle line change when hover the cursor
+  $(".header__toggle").hover(
+    function () {
+      $(".toggle__line:first-child").removeClass("toggle--original");
+      $(".toggle__line:last-child").removeClass("toggle--original");
+      $(".toggle__line:first-child").addClass("toggle--animate");
+      $(".toggle__line:last-child").addClass("toggle--animate");
+    },
+    function () {
+      $(".toggle__line:first-child").removeClass("toggle--animate");
+      $(".toggle__line:last-child").removeClass("toggle--animate");
+      $(".toggle__line:first-child").addClass("toggle--original");
+      $(".toggle__line:last-child").addClass("toggle--original");
+    }
+  );
+
   //menu slides open
   $(".header__toggle").click(function () {
     $(".header__menu-wrap")
@@ -55,15 +71,16 @@ $(function () {
   $(".arrow__next").click(function () {
     $(".main__slides-wrap").animate(
       {
-        // left: -1590,
-        left: "-82.812%",
+        //left: -1625,
+        //(main-slides-wrap left + main__slide width + margin-right)
+        left: "-85.5%",
       },
       1100,
       function () {
         $(".main__slides-wrap")
           .css({
-            // left: -550,
-            left: "-28.645%",
+            //            left: -590
+            left: "-30.729%",
           })
           .find(".main__slide:first")
           .appendTo(".main__slides-wrap");
@@ -79,13 +96,13 @@ $(function () {
   $(".arrow__prev").click(function () {
     $(".main__slides-wrap")
       .css({
-        // left: -1500,
-        left: "-82.812%",
+        //         left: -1500,
+        left: "-95%",
       })
       .animate(
         {
           // left: -550,
-          left: "-28.645%",
+          left: "-30.729%",
         },
         1100
       )
@@ -98,20 +115,37 @@ $(function () {
   });
 
   // next arrow auto-play
-  // setInterval(function () {
-  //   $(".arrow__next").trigger("click");
-  // }, 5000);
+  setInterval(function () {
+    $(".arrow__next").trigger("click");
+  }, 5000);
 
-  //unicef sub title hover
-  $(".unicef__img").hover(function () {
-    $(".unicef__title").fadeOut(500);
-    $(".unicef__sub").fadeIn(500);
+  //unicef text-box appear when scrolling down to'unicef'section
+  var unicefPosTop = $("#unicef").offset().top;
+
+  $(window).scroll(function () {
+    var sct = $(this).scrollTop();
+
+    if (sct > unicefPosTop - 300) {
+      $(".unicef__text-box").animate(
+        {
+          left: "20%",
+          opacity: 1,
+        },
+        2000
+      );
+    }
   });
 
   //product swiper
-  var swiper = new Swiper(".product-swiper-container", {
-    slidesPerView: 3,
-    spaceBetween: 20,
+  var productswiper = new Swiper(".product-swiper-container", {
+    breakpoints: {
+      960: {
+        slidesPerView: 3,
+      },
+      500: {
+        slidesPerView: 2,
+      },
+    },
     loop: true,
     pagination: {
       el: ".swiper-pagination",
