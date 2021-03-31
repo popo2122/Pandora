0bcb30b40610ddc8b294840db66d3d9ab472cbbe
Jay Shin <75009488+popo2122@users.noreply.github.com>
2021-03-31 22:43:11 +0900

    modified unicef section text animation position

--- js/tab_mobile.js
+++ js/tab_mobile.js
@@ -1,41 +1,65 @@
 $(function () {
-  //tab & mobile main swiper
-  var swiper = new Swiper(".main-swiper-container", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    grabCursor: true,
-  });
+    //tab & mobile main swiper
+    var mainswiper = new Swiper(".main-swiper-container", {
+        breakpoints: {
+            960: {
+                slidesPerView: 1.5,
+            },
+            500: {
+                slidesPerView: 1
+            }
+        },
+        centeredSlides: true,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        speed: 1500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        grabCursor: true,
+    });
 
-  //when hover the unicef section, a img is fade-in
-  hover = true;
-  $(".mo-unicef__text-box").hover(function () {
-    if (hover == true) {
-      $(".mo-unicef__title").removeClass("unicef__title--moving");
-      $(".mo-unicef__img>img").attr({
-        src: "img/mobile_unicef_hover_bg.png",
-      });
-      $(".mo-unicef__text-box").stop().animate(
-        {
-          left: "15%",
-        },
-        1000
-      );
-      $(".mo-unicef__desc,.mo-unicef__btn").stop().animate(
-        {
-          opacity: 1,
-        },
-        1200
-      );
+    //when hover the unicef section, a img is fade-in
+
+    function textMoving(leftPos) {
+        $(".mo-unicef__title").removeClass("unicef__title--moving");
+        $(".mo-unicef__img>img").attr({
+            src: "img/mobile_unicef_hover_bg.png",
+        });
+        $(".mo-unicef__text-box").stop().animate({
+                left: leftPos,
+            },
+            1000
+        );
+        $(".mo-unicef__desc,.mo-unicef__btn").stop().animate({
+                opacity: 1,
+            },
+            1200
+        );
     }
-    hover = false;
-  });
+
+    $(window).resize(function () {
+        ww = $(window).width();
+        if(ww >= 960){
+            textMoving('15%');
+        } else if(ww < 960){
+            textMoving('25%');
+        }
+    });
+
+    hover = true;
+    $(".mo-unicef__text-box").hover(function () {
+        ww = $(window).width();
+        if (hover == true && ww >= 960) {
+            textMoving('15%');
+        } else if (hover == true && ww < 960) {
+            textMoving('25%');
+        }
+        hover = false;
+    });
 }); //end
